4f566b1da5e1cd2e09911699cc1a54a1f56c2f41
Artur Rodrigues <artur190503@outlook.com>
2024-06-03 15:21:06 -0300

    More tests

--- teste.dart
+++ teste.dart
@@ -1,13 +1,24 @@
+// Rodar o código: dart nome.dart
+// Comentarios !!
+
+import 'dart:io';
+
 main (List aux) {
-  // Rodar o código: dart nome.dart
-  // Comentarios
+  // Funções
   print(aux);
-  print("Hello, World!");
+
+  // Variáveis
+  print('Hello, World!');
 
   int a = 10;
   double b = 10.5;
-  String c = "Hello, World!";
+  String c = 'Hello, World!';
 
   print('O valor de a é: $a\nO valor de b é: $b\nO valor de c é: $c');
+
+  // Entrada de dados
+  print('\nDigite um texto: ');
+  var text = stdin.readLineSync();
+  print('\nO texto digitado foi: $text\n');
 
 }
