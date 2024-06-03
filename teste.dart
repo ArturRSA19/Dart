9d01997d158c2e56ad7bf8666f834c34a796439a
Artur Rodrigues <artur190503@outlook.com>
2024-06-03 15:07:13 -0300

    Test

--- teste.dart
+++ teste.dart
@@ -1,3 +1,13 @@
-main () {
-    print("Hello, World!");
+main (List aux) {
+  // Rodar o código: dart nome.dart
+  // Comentarios
+  print(aux);
+  print("Hello, World!");
+
+  int a = 10;
+  double b = 10.5;
+  String c = "Hello, World!";
+
+  print('O valor de a é: $a\nO valor de b é: $b\nO valor de c é: $c');
+
 }
